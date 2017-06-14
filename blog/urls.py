71ff6f67f0878d2e2b3e0cf15b2daa50f5999447
sudhakaranp <sudhakaran@helyxon.com>
2017-06-14 11:47:57 +0530

    initial committ

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from .views import *
+
+urlpatterns = [
+    url(r'^$', post_list, name='post_list'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', post_detail, name='post_detail'),
+    url(r'^(?P<post_id>\d+)/share/$', post_share, name='post_share'),
+]
